5203d6c65c17ec69b9998e164e5d7a0c9eafd9dd
donovanwest <donovanrwest@gmail.com>
2019-09-20 14:30:11 -0500

    Fruit Inheritence

---
+++ DoesItBlend/IBlendable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DoesItBlend
+{
+    /// <summary>
+    /// Interface for items that can be blended
+    /// </summary>
+    interface IBlendable
+    {
+        /// <summary>
+        /// blends the object
+        /// </summary>
+        /// <returns>a string describing the blended object.</returns>
+        string Blend();
+    }
+}
